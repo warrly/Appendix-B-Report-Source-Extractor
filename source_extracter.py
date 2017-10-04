bb02643a80cd32eda096b5388f4903e238f4907c
Daniel Metz <danielmmetz@gmail.com>
2017-10-03 20:59:15 -0700

    day 1

---
+++ source_extracter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Call this file as:
+    ./source_extracter.py <input file>
+"""
+import fileinput
+import re
+
+
+PATTERNS = [
+    # Conversations (Conversation with First (optional-Middle) Last)
+    re.compile('Conversation with [A-Z][a-zA-Z]+(?: [A-Z][a-zA-Z]+){1,2}'),
+    # Depositions (Deposition of Firstname (optional-Middle) Last, Month Day, Year)
+    re.compile('Deposition of [A-Z][a-zA-Z]+(?: [A-Z][a-zA-Z]+){1,2}, [A-Z][a-z]+ \d{1,2}, \d{4}'),
+    # Bates sources (EMAILS 12345678)
+    re.compile('[A-Z]+ [0-9]{8}'),
+]
+
+
+def sources_of(footnote: str) -> [str]:
+    sources = []
+    for pattern in PATTERNS:
+        results = pattern.findall(footnote)
+        for result in results:
+            sources.append(f'{result}.')
+    return sources
+
+
+if __name__ == '__main__':
+    for i, footnote in enumerate(fileinput.input()):
+        sources = sources_of(footnote)
+        for source in sources:
+            print(f'{i+1}\t{source}')
